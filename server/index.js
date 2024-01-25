db7aedbec19180a9e107049ee3afa38f54f8e605
devanshhhhh <84448303+devanshhhhh@users.noreply.github.com>
2024-01-26 01:55:24 +0530

    removed cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express=require("express");
 const dotenv=require("dotenv");
-const cors=require("cors");
 dotenv.config();
 
 const app=express();
@@ -8,8 +7,6 @@ const routes=require("./routes/auth.js");
 
 app.use(express.json());
 app.use("/app", routes);
-
-app.use(cors());
 
 const PORT=process.env.PORT;
 app.listen(PORT);
